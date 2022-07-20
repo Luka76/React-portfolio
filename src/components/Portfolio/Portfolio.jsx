fb9c186d81fc02b53dea0fc566c09b4671491666
Luka76 <vargaluka76@gmail.com>
2022-07-20 16:32:47 +0200

    link updates

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import img1 from "../../assets/portfolio-1.jpg"
 import img2 from "../../assets/portfolio-2.jpg"
 import img3 from "../../assets/portfolio-3.jpg"
 import img4 from "../../assets/portfolio-4.jpg"
-import img5 from "../../assets/portfolio-5.jpg"
+// import img5 from "../../assets/portfolio-5.jpg" food order image
 import img6 from "../../assets/portfolio-6.jpg"
 
 const data = [
@@ -12,42 +12,42 @@ const data = [
     id: 1,
     image: img1,
     title: "College instructions",
-    github: "https://github.com",
+    github: "https://github.com/Luka76/konzultacije",
     demo: "https://dribbble.com"
   },
   {
     id: 2,
     image: img2,
     title: "Hair saloon website",
-    github: "https://github.com",
+    github: "https://github.com/Luka76/M.A.K-website",
     demo: "https://dribbble.com"
   },
   {
     id: 3,
     image: img3,
-    title: "JavaScript slider",
-    github: "https://github.com",
+    title: "HTML and CSS learning",
+    github: "https://github.com/Luka76/Slider_example",
     demo: "https://dribbble.com"
   },
   {
     id: 4,
-    image: img4,
-    title: "News website",
-    github: "https://github.com",
+    image: img3,
+    title: "Slider",
+    github: "https://github.com/Luka76/Solution",
     demo: "https://dribbble.com"
   },
   {
     id: 6,
-    image: img6,
-    title: "Portfolio project",
-    github: "https://github.com",
+    image: img4,
+    title: "News website",
+    github: "https://github.com/Luka76/First-react-app",
     demo: "https://dribbble.com"
   },
   {
     id: 5,
-    image: img5,
-    title: "Food delivery",
-    github: "https://github.com",
+    image: img6,
+    title: "Portfolio project",
+    github: "https://github.com/Luka76/React-portfolio",
     demo: "https://dribbble.com"
   },
 ]
